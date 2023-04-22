bbe753e8d2eb89f00ac4be69fbee8612dd5d23f6
Threshu <73550807+Threshu@users.noreply.github.com>
2023-04-22 17:24:53 +0000

    install vue-virtual-scroller-pckg

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 import { createApp } from "vue";
 import "tw-elements";
 import "./tailwind.css";
+import VueVirtualScroller from "vue-virtual-scroller";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.use(VueVirtualScroller);
+app.mount("#app");

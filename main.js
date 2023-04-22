7cb18363f6f313f4eb1f20a28edd0848b4548944
Threshu <73550807+Threshu@users.noreply.github.com>
2023-04-22 18:28:08 +0000

    virual scroller styles

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
+import "vue-virtual-scroller/dist/vue-virtual-scroller.css";
+
 import { createApp } from "vue";
 import "tw-elements";
 import "./tailwind.css";
 import VueVirtualScroller from "vue-virtual-scroller";
+
 import App from "./App.vue";
 
 const app = createApp(App);
-
 app.use(VueVirtualScroller);
 app.mount("#app");
